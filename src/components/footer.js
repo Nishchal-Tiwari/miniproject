850b2a85bd4c7d142997c9971ec9c8e733c68ff2
Nishchal Tiwari <73414722+Nishchal-Tiwari@users.noreply.github.com>
2023-04-28 12:24:03 +0530

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import '../App.css'
+export default function footer() {
+  return (
+   
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Poster Bazzar<i>Welcome to our online store</i> iWe offer an extensive range of posters featuring different styles, themes, and genres to cater to all your aesthetic needs.Whether you're looking for motivational quotes, inspirational images, breathtaking landscapes, or iconic celebrities, our poster collection has it all. You can choose from various sizes, shapes, and frames to suit your preferences and decor style.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">Motivational</a></li>
+              <li><a href="#">Relegious</a></li>
+              <li><a href="#">Gaming</a></li>
+              <li><a href="#">Abstract</a></li>
+              <li><a href="#">Custom</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Contribute</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr/>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+         <a href="#">Poster Bazzar</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+  )
+}
